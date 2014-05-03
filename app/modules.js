d90b45aebfdad6f3d82f7d777c39821973a10e5b
Elvis Luciano <elvis.gs@gmail.com>
2014-05-03 16:29:40 -0300

    Clear modules tests

--- app/modules.js
+++ app/modules.js
@@ -1,9 +1,20 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
+	var Module = function(greeting, name) {
+		this.greeting = greeting;
+		this.name = name;
+	};
+
+	Module.prototype = {
+		sayIt: function() {
+			return this.greeting + ', ' + this.name;
+		}
+	};
+    	
   return {
     createModule : function(str1, str2) {
-
+    	return new Module(str1, str2);
     }
   };
 });
