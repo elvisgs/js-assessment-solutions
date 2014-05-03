bacfbd512b600a3d16019feb680fc3c1ecf2bc87
Elvis Luciano <elvis.gs@gmail.com>
2014-05-03 18:28:44 -0300

    Clear counter tests

--- app/count.js
+++ app/count.js
@@ -3,6 +3,22 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+    	var current = start;
+
+    	var timer = setInterval(function() {
+    		console.log(current);
+    		current++
+
+    		if (current > end) {
+    			clearInterval(timer);
+    		}
+    	}, 100);
+
+    	return {
+    		cancel: function() {
+    			clearInterval(timer);
+    		}
+    	}
     }
   };
 });
