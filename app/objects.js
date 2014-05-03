9e1106c0fb3829b32ca1eeacaf2ba6f9d91321af
Elvis Luciano <elvis.gs@gmail.com>
2014-05-03 16:55:41 -0300

    Clear objects and context tests

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+    	return fn.call(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-
+    	constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
+    	var res = [];
 
+    	for (var i in obj) {
+    		if (obj.hasOwnProperty(i)) {
+    			res.push(i + ': ' + obj[i]);
+    		}
+    	}
+
+    	return res;
     }
   };
 });
