5e43f27001478d8df158339825cf9d0ace52d304
Elvis Luciano <elvis.gs@gmail.com>
2014-05-04 21:32:57 -0300

    Clear recursion tests

--- app/recursion.js
+++ app/recursion.js
@@ -1,10 +1,38 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
+	function list(data) {
+		var files = [];
+
+		data.files && data.files.forEach(function(file) {
+			if (typeof file === 'string') {
+				files.push(file);
+			} else {
+				files = files.concat(list(file));
+			}
+		});
+
+		return files;
+	}
+  
   return {
     listFiles: function(data, dirName) {
+			if (!data.files) return [];
 
-    },
+			dirName = dirName || data.dir;	
+
+			if (data.dir === dirName) {
+				return list(data);
+			}
+			else {
+				for (var i = 0; i < data.files.length; i++) {
+					var file = data.files[i];
+					if (typeof file === 'object') {
+						return this.listFiles(file, dirName);
+					}
+				}
+			}
+	  },
 
     permute: function(arr) {
 
