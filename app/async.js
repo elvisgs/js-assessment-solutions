32c4523225f164af2ff063aa72643d11b5ccce8e
Elvis Luciano <elvis.gs@gmail.com>
2014-05-05 10:02:58 -0300

    Clear async tests

--- app/async.js
+++ app/async.js
@@ -3,11 +3,27 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
+    	var dfd = $.Deferred();
 
+    	setTimeout(function() {
+    		dfd.resolve(value);
+    	}, 100);
+    	
+    	return dfd;
     },
 
     manipulateRemoteData : function(url) {
+    	var dfd = $.Deferred();
 
+    	$.getJSON(url).then(function(json) {
+    		var names = json.people.map(function(person) {
+    			return person.name;
+    		}).sort();
+
+    		dfd.resolve(names);
+    	});
+
+    	return dfd;
     }
   };
 });
